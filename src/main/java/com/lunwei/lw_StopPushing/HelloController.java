73e81f1571766431fa5d634aa6a99e365a26dd2c
wangze <wz547993630@163.com>
2020-01-10 14:16:23 +0800

    加入定时任务

--- src/main/java/com/lunwei/lw_StopPushing/HelloController.java
+++ src/main/java/com/lunwei/lw_StopPushing/HelloController.java
@@ -3,6 +3,8 @@ package com.lunwei.lw_StopPushing;
 import com.lunwei.lw_StopPushing.model.User;
 import com.lunwei.lw_StopPushing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,29 +13,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Component
 public class HelloController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello lw_StopPushing";
-    }
-
-    @GetMapping
-    @RequestMapping("/getUser")
-    public User getUser(){
-        User user = userService.selectByPrimaryKey(1);
-        return user;
-    }
-
-    @GetMapping
-    @RequestMapping("/getList")
-    public List<User> getList(){
-        List<User> list = userService.getList();
-        return list;
+    @RequestMapping("list")
+    @Scheduled(cron = "*/5 * * * * ?")
+    public Object list(){
+        System.out.println(123);
+        return 123;
     }
 
 }
